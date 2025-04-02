8b6c49c7c77fada012afe4bf30a7a972db6d56bb
Juan I. Giorgetti <juan.giorgetti@itk-engineering.com>
2025-04-02 17:46:50 +0200

    Assignment 9 complete

--- base_external/package/aesd-char-driver/aesd-char-driver.mk
+++ base_external/package/aesd-char-driver/aesd-char-driver.mk
@@ -1,23 +1,24 @@
 
 ##############################################################
 #
-# LDD
+# AESD_CHAR_DRIVER
 #
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-LDD_VERSION = 'c59d59a7520f79c66934709ed23d0e4cfee88940'
-LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-juani-g16.git'
-LDD_SITE_METHOD = git
-LDD_GIT_SUBMODULES = YES
-LDD_MODULE_SUBDIRS = scull misc-modules
+AESD_CHAR_DRIVER_VERSION = 'c3166062d1e881eb32ae564398c29918de9385a5'
+AESD_CHAR_DRIVER_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-juani-g16.git'
+AESD_CHAR_DRIVER_SITE_METHOD = git
+AESD_CHAR_DRIVER_GIT_SUBMODULES = YES
+AESD_CHAR_DRIVER_MODULE_SUBDIRS = aesd-char-driver
+AESD_CHAR_DRIVER_MODULE_NAME = aesd-char-driver
+AESD_CHAR_DRIVER_MODULE_VERSION = 1.0
 
-define LDD_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
+define AESD_CHAR_DRIVER_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
+
